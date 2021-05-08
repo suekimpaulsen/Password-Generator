fe530b5c7b48229cc3af2c04e9237fd322018674
Sue Kim Paulsen <suekimpaulsen@gmail.com>
2021-05-07 20:23:11 -0500

    working for loops

--- Develop/script.js
+++ Develop/script.js
@@ -39,69 +39,32 @@ function generatePassword() {
     }
     switch (lowercaseIncluded) {
       case true:
-        possibleChars = [].concat(possibleChars, LowerCase);
+        possibleChars = [].concat(possibleChars, lowercase);
         break;
     }
     switch (uppercaseIncluded) {
       case true:
-        possibleChars = [].concat (possibleChars, UpperCase);
+        possibleChars = [].concat (possibleChars, uppercase);
         break;
     }
     switch (specialcharIncluded) {
       case true:
-        possibleChars = [].concat (possibleChars, SpecialCharacters);
+        possibleChars = [].concat (possibleChars, specialCharacters);
         break;
     }
     console.log(numericIncluded, lowercaseIncluded, uppercaseIncluded, specialcharIncluded);
     console.log(possibleChars);
+    
+    var newPassword = ''
 
     for (var i = 0; i < PasswordLength; i++) {
-      var RandomPassword = possibleChars[Math.floor(Math.random() * PasswordLenght)];
-      console.log(RandomPassword);
+      var randomPassword = possibleChars[Math.floor(Math.random() * possibleChars.length)];
+      console.log (randomPassword)
+      newPassword += randomPassword
     }
-
-    // for (var i = 0; i < PasswordLength; i++) {
-    //   var possibleChars = [];
-    //   switch (numericIncluded) {
-    //     case true:
-    //       possibleChars = numeric;
-    //       break;
-    //   }
-    //   switch (lowercaseIncluded) {
-    //     case true:
-    //       possibleChars = [].concat(possibleChars, LowerCase);
-    //       break;
-    //   }
-    //   switch (uppercaseIncluded) {
-    //     case true:
-    //       possibleChars = [].concat (possibleChars, UpperCase);
-    //       break;
-    //   }
-    //   switch (specialcharIncluded) {
-    //     case true:
-    //       possibleChars = [].concat (possibleChars, SpecialCharacters);
-    //       break;
-    //   }
-    //   var RandomPassword = possibleChars[Math.floor(Math.random() * PasswordLength)];
-    //   console.log(RandomPassword);
-    // }
-    // console.log(numericIncluded, lowercaseIncluded, uppercaseIncluded, specialcharIncluded);
-    // console.log(possibleChars);
-  }
-  
-
-
-  // for (var i = 0; i < PasswordLength; i++) {
-  //   function possibleChars([]) {
-  //     switch (possibleChars)
-   
-  //   var RandomPassword = possibleChars[Math.floor(Math.random() * PasswordLength)];
-  //   console.log(RandomPassword[i]);
-  // }
-  // console.log(possibleChars);
   return "result"
 };
 var numeric = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
-var LowerCase = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
-var UpperCase = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
-var SpecialCharacters = ['@', '&', '+', '/', '!', '#', '$', '^', '?', ';', ':', ',', '(', ')', '{', '}', '[', ']', '~', '-', '_'];
+var lowercase = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+var uppercase = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+var specialCharacters = ['@', '&', '+', '/', '!', '#', '$', '^', '?', ';', ':', ',', '(', ')', '{', '}', '[', ']', '~', '-', '_'];
